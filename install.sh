3a6c41735b3df3668eb4d0576e2ff718dbf9e434
Atalay Donat <atalaydonatt@gmail.com>
2025-04-23 14:10:59 +0200

    Small changes in install script

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
-cd custom_robosuite
-pip install -e .
-cd ..
-
-cd custom_robocasa
-pip install -e .
-cd ..
+pip install -e custom_robosuite/
+pip install -e custom_robocasa/
 
 pip install -r requirements.txt
 
@@ -12,5 +7,3 @@ python -m robocasa.scripts.download_kitchen_assets
 
 python -m robosuite.scripts.setup_macros
 python -m robocasa.scripts.setup_macros
-
-pip install "git+https://github.com/facebookresearch/pytorch3d.git" # takes a while
