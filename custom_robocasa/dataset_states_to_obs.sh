c24d9a2d2ed563a4126cd3f740a2c434e1627f6a
Tschian <wangtschian@gmail.com>
2026-03-06 22:58:53 +0100

    ..

--- custom_robocasa/dataset_states_to_obs.sh
+++ custom_robocasa/dataset_states_to_obs.sh
@@ -5,10 +5,11 @@
 #SBATCH -n 1
 #SBATCH -t 10:00:00
 #SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1 # Define the paths for storing output and error files
 #SBATCH --output=/home/hk-project-p0024023/mn4777/depth_vla/slurm_jobs/robocasa/%x_%j.out
 #SBATCH --error=/home/hk-project-p0024023/mn4777/depth_vla/slurm_jobs/robocasa/%x_%j.err
 
-eval "$(conda shell.bash hook)"
+source ~/.bashrc
 conda activate robocasa
 
 export LD_LIBRARY_PATH=$HOME/miniconda3/lib:$LD_LIBRARY_PATH
